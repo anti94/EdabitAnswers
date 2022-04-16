7bb38cf84dcbfacd4d31bf991bff7c4894085f6e
“[Caglayan <caglayangulucan@gmail.com>
2022-04-16 22:28:19 +0300

    First three questions's answers have been added.

--- Questions/Program.cs
+++ Questions/Program.cs
@@ -16,11 +16,35 @@ namespace Questions
         }
 
 
-        //question1
+        /// <summary>
+        /// Question_1: Write a function that returns the string "something" joined with a space " " and the given argument a.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
         public static string GiveMeSomething(string a)
         {
             return "something" + " " + a;
         }
+
+        /// <summary>
+        /// Question_2: Write a function that takes the base and height of a triangle and return its area.
+        /// </summary>
+        /// <param name="b"></param>
+        /// <param name="h"></param>
+        /// <returns></returns>
+        public static int triArea(int b, int h)
+        {
+            return b * h / 2;
+        }
+
+        /// <summary>
+        /// Question_3: Write a function that takes an integer minutes and converts it to seconds.
+        /// </summary>
+        public static int Convert(int minutes) 
+        { 
+            return minutes * 60;
+        }
+
 
 
 
