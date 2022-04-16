5db11824f8ec0201757b0089e0183378804ef864
“[Caglayan <caglayangulucan@gmail.com>
2022-04-16 21:30:15 +0300

    initial commit

---
+++ Questions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GiveMeSomething("caglayan"));
+
+            Console.ReadLine();
+        }
+
+
+        //question1
+        public static string GiveMeSomething(string a)
+        {
+            return "something" + " " + a;
+        }
+
+
+
+    }
+}
